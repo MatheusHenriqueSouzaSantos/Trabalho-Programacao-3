980a50e8f846372d749b6947154acf2aa85d6396
matheusHen <matheushenriquesouzasantos2@gmail.com>
2025-11-04 22:22:27 -0300

    testando validação

--- DTO/TransacaoDTO.cs
+++ DTO/TransacaoDTO.cs
@@ -1,18 +1,48 @@
-﻿namespace umfgcloud.programcaoiii.vendas.api.DTO
+﻿using umfgcloud.programcaoiii.vendas.api.Entidades;
+
+namespace umfgcloud.programcaoiii.vendas.api.DTO
 {
     public sealed class TransacaoDTO
     {
-        public sealed class TransacaoCapaRequest
+        public class TransacaoCapaRequest
         {
             public Guid IdCliente { get; set; } = Guid.Empty;
 
             public Guid IdVendedor { get; set; } = Guid.Empty;
+
+            public TransacaoCapaRequest(string idCliente, string idVendedor)
+            {
+                Guid idConvertidoCliente;
+                if (!Guid.TryParse(idCliente, out idConvertidoCliente))
+                {
+                    throw new ArgumentException("id no formato inválido de GUID");
+                }
+                Guid idConvertidoVendedor;
+                if (!Guid.TryParse(idVendedor, out idConvertidoVendedor))
+                {
+                    throw new ArgumentException("id no formato inválido de GUID");
+                }
+                IdCliente = idConvertidoCliente;
+                IdVendedor = idConvertidoVendedor;
+            }
         }
 
-        public sealed class TransacaoItemRequest
+        public class TransacaoItemRequest
         {
             public Guid IdProduto { get; set; } = Guid.Empty;
             public decimal Quantidade { get; set; } = 0;
+
+            public TransacaoItemRequest(string idProduto, decimal quantidade)
+            {
+
+                Guid idConvertidoProduto;
+                if (!Guid.TryParse(idProduto, out idConvertidoProduto))
+                {
+                    throw new ArgumentException("id no formato inválido de GUID");
+                }
+                IdProduto = idConvertidoProduto;
+                Quantidade = quantidade;
+            }
         }
     }
 }
